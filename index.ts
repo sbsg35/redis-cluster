434f20b02b1a47dbe49d989f46348c8157ac1b57
sbsg35 <ssgh920@gmail.com>
2023-11-16 15:05:44 +1100

    use redis util script

--- index.ts
+++ index.ts
@@ -1,30 +1,30 @@
-import { createClient, createCluster } from "redis";
+import * as Redis from "ioredis";
 
-const redisCluster = async () => {
-  const cluster = createCluster({
-    rootNodes: [
-      { url: "redis://localhost:7000" },
-      { url: "redis://localhost:7001" },
-      { url: "redis://localhost:7002" },
-    ],
-    useReplicas: true,
-  });
-
-  await cluster.connect();
-
-  cluster.on("error", (err) => console.log("Redis Cluster Error", err));
-
-  console.log(JSON.stringify(cluster.shards, null, 2));
-
-  await cluster.disconnect();
+const myF = async () => {
+  const cluster = new Redis.Cluster([
+    {
+      port: 7000,
+      host: "127.0.0.1",
+    },
+    {
+      port: 7001,
+      host: "127.0.0.1",
+    },
+    {
+      port: 7002,
+      host: "127.0.0.1",
+    },
+  ]);
+  try {
+    cluster.on("node error", (err) => console.error(err));
+    const d = await cluster.set("name123", "raj");
+    console.log(await cluster.get("name123"));
+    console.log({ d });
+  } catch (error) {
+    console.error({ error });
+  } finally {
+    cluster.quit();
+  }
 };
 
-// redisCluster();
-
-const redisClient = async () => {
-  const client = createClient({ url: "redis://localhost:7000" });
-  await client.connect();
-  await client.set("name", "raj");
-  await client.keys("*");
-  await client.disconnect();
-};
+myF();
